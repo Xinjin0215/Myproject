c199c11ca757aac52f74a9aba5d9d2d62994cdcb
Xin Jin <xinjin0215@gmail.com>
2018-10-28 20:58:41 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Button } from 'reactstrap';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App"><h1>
+        Ovals Project</h1>
+
+        <Button color="danger">Click Me!</Button>
+
+      </div>
+    );
+  }
+}
+
+export default App;
